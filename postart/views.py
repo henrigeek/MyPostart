cbbaa1d77a9ba29c1bf9d9d7e315bcc935a9fd9a
henrigeek <henri.augris@gmail.com>
2015-02-22 18:12:50 -0500

    access key after bucket

--- postart/views.py
+++ postart/views.py
@@ -10,9 +10,10 @@ from django.contrib import auth
 from django.utils.decorators import method_decorator
 from django.core.files.storage import default_storage
 from boto.s3.connection import S3Connection
-conn = S3Connection(ACCESS_KEY,SECRET_KEY)
 bucket = conn.create_bucket('postartnyc')
 from boto.s3.key import Key
+conn = S3Connection(ACCESS_KEY,SECRET_KEY)
+
 
 def logout_view(request):
     auth.logout(request)
